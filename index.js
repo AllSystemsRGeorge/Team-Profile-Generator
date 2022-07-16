9fd9c5ddd7cfb4b61c88d908266070bb3d60b211
AllSystemsRGeorge <96609501+AllSystemsRGeorge@users.noreply.github.com>
2022-07-15 22:15:04 -0700

    Created new files and working on JS

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-const generateHTML = require
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
