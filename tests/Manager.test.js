c1938041846e80317756667d92acfbe4b7530336
AllSystemsRGeorge <96609501+AllSystemsRGeorge@users.noreply.github.com>
2022-07-15 21:04:35 -0700

    fixing manager test

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,39 +1,44 @@
-const Manager = require('../lib/manager');
+const Manager = require('../lib/manager.js');
 
 describe("Manager", () => {
     describe('Initialization', () => {
-        it('should initiate an object', () => {
-            const obj = new Employee();
-            expect(typeof(obj)).toEqual('object');
+        it('should create an object', () => {
+            const manager = new Manager('John Doe', 1, 'something@gmail.com', '101');
+            expect(typeof(manager)).toEqual('object');
+        });
+    
+        it('should take a name, id, email, office number, and set them as properties on an object', ()=> {
+            const name = 'John Doe';
+            const id = 1;
+            const email = 'johndoe@something@gmail.com';
+            const officeNumber = '427'
+            const manager = new Manager(name, id, email, officeNumber);
+            expect(manager.name).toEqual(name);
+            expect(manager.id).toEqual(id);
+            expect(manager.email).toEqual(email);
+            expect(manager.officeNumber).toEqual(officeNumber);
         });
     });
 
-    describe('Name', () => {
-        it('should create new name', () => {
-            const obj = new Employee('George');
-            expect(typeof(obj)).toEqual('George');
+    describe('getOfficeNumber', () => {
+        it('should return office number passed to the constructor', () => {
+            const name = 'John Doe';
+            const id = 1;
+            const email = 'johndoe@something@gmail.com';
+            const officeNumber = '427'
+            const manager = new Manager(name, id, email, officeNumber);
+            expect(manager.getOfficeNumber()).toEqual(officeNumber);
         });
     });
 
-    describe('Id', () => {
-        it('should create new id', () => {
-            const obj = new Id('123');
-            expec(typeof(obj)).toEqual('123');
+    describe('getRole', () => {
+        it('should return "Manager"', () => {
+            const name = 'John Doe';
+            const id = 1;
+            const email = 'johndoe@something@gmail.com';
+            const officeNumber = '427'
+            const manager = new Manager(name, id, email, officeNumber);
+            expect(manager.getRole()).toEqual('Manager');
         });
     });
-
-    describe('Email', () => {
-        it('should create new email', () => {
-            const obj = new Id('something@gmail.com');
-            expec(typeof(obj)).toEqual('something@gmail.com');
-        });
-    });
-
-    describe('officeNumber', () => {
-        it('should create new email', () => {
-            const obj = new Id('427');
-            expec(typeof(obj)).toEqual('427');
-        });
-    });
-
 });
